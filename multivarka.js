9e23e0e0d0491f4f9b9e39cf1001e10a2c7168fd
Dolgih Natalia <n.e.dolgih@yandex.ru>
2016-03-04 16:17:37 +0500

    допы и фиксы

--- multivarka.js
+++ multivarka.js
@@ -4,6 +4,7 @@ var MongoClient = mongodb.MongoClient;
 var Connection = function (url) {
     this.url = url;
     this.operations = {};
+    this.isNot = false;
 };
 
 module.exports.server = function (url) {
@@ -21,60 +22,102 @@ Connection.prototype.where = function (searchField) {
 };
 
 Connection.prototype.equal = function (value) {
-    if (!this.not) {
+    if (!this.isNot) {
         this.operations[this.field] = value;
     } else {
         this.operations[this.field] = {$ne: value};
-        this.not = false;
+        this.isNot = false;
     }
     return this;
 };
 
 Connection.prototype.lessThan = function (value) {
-    if (!this.not) {
+    if (!this.isNot) {
         this.operations[this.field] = {$lt: value};
     } else {
         this.operations[this.field] = {$gte: value};
-        this.not = false;
+        this.isNot = false;
     }
     return this;
 };
 
 Connection.prototype.greatThan = function (value) {
-    if (!this.not) {
+    if (!this.isNot) {
         this.operations[this.field] = {$gt: value};
     } else {
         this.operations[this.field] = {$lte: value};
-        this.not = false;
+        this.isNot = false;
     }
     return this;
 };
 
 Connection.prototype.include = function (values) {
-    if (!this.not) {
+    if (!this.isNot) {
         this.operations[this.field] = values;
     } else {
         this.operations[this.field] = {$ne:values};
-        this.not = false;
+        this.isNot = false;
     }
     return this;
 };
 
+Connection.prototype.set = function (val) {
+    this.setValue = val;
+    return this;
+};
+
 Connection.prototype.not = function () {
-    this.not = true;
+    this.isNot = true;
     return this;
 };
 
 Connection.prototype.find = function (callback) {
-    var this_ = this;
-    MongoClient.connect(this.url, function (err, db) {
+    dbRequest('find', this, callback);
+};
+
+Connection.prototype.remove = function (callback) {
+    dbRequest('remove', this, callback);
+};
+
+Connection.prototype.update = function (callback) {
+    dbRequest('update', this, callback);
+};
+
+Connection.prototype.insert = function (obj, callback) {
+    this.insertObj = obj;
+    dbRequest('update', this, callback);
+};
+
+function dbRequest(func, connectionInfo, callback) {
+    MongoClient.connect(connectionInfo.url, function (err, db) {
         if (err) {
-            callback(err);
+            console.log(err)
         } else {
-            var table = db.collection(this_.table);
-            var result = table.find(this_.operations);
-            db.close();
+            var table = db.collection(connectionInfo.table);
+            var result;
+            switch (func) {
+                case('update'): {
+                    result = table.func(connectionInfo.operations, connectionInfo.setValue).toArray();
+                    break;
+                }
+                case('remove'): {
+                    result = table.deleteMany(connectionInfo.operations).toArray();
+                    break;
+                }
+                case('find'):{
+                    result = table.find(connectionInfo.operations).toArray();
+                    break;
+                }
+                case('insert'):{
+                    result = table.insertOne(connectionInfo.insertObj).toArray();
+                    break
+                }
+                default:{
+                    callback('err')
+                }
+            }
+            result.then(()=>{db.close();});
             callback(null, result);
         }
     });
-};
+}
